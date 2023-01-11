19101722b56c46f771923303b811b025cc443c77
scx567888 <scx567888@outlook.com>
2023-01-11 14:01:35 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/douyin/proto_entity/webcast/im/UpdateFanTicketMessageOuterClass.java
+++ src/main/java/cool/scx/live_room_watcher/douyin/proto_entity/webcast/im/UpdateFanTicketMessageOuterClass.java
@@ -29,18 +29,16 @@ public final class UpdateFanTicketMessageOuterClass {
   static {
     java.lang.String[] descriptorData = {
       "\n\'webcast/im/UpdateFanTicketMessage.prot" +
-      "o\032\030webcast/im/Message.proto\032\027webcast/im/" +
-      "Common.proto\"\202\001\n\026UpdateFanTicketMessage\022" +
-      "\027\n\006common\030\001 \001(\0132\007.Common\022\036\n\026roomFanTicke" +
-      "tCountText\030\002 \001(\t\022\032\n\022roomFanTicketCount\030\003" +
-      " \001(\003\022\023\n\013forceUpdate\030\004 \001(\010B=\n9cool.scx.li" +
-      "ve_room_watcher.douyin.proto_entity.webc" +
-      "ast.imP\001b\006proto3"
+      "o\032\027webcast/im/Common.proto\"\202\001\n\026UpdateFan" +
+      "TicketMessage\022\027\n\006common\030\001 \001(\0132\007.Common\022\036" +
+      "\n\026roomFanTicketCountText\030\002 \001(\t\022\032\n\022roomFa" +
+      "nTicketCount\030\003 \001(\003\022\023\n\013forceUpdate\030\004 \001(\010B" +
+      "=\n9cool.scx.live_room_watcher.douyin.pro" +
+      "to_entity.webcast.imP\001b\006proto3"
     };
     descriptor = com.google.protobuf.Descriptors.FileDescriptor
       .internalBuildGeneratedFileFrom(descriptorData,
         new com.google.protobuf.Descriptors.FileDescriptor[] {
-          cool.scx.live_room_watcher.douyin.proto_entity.webcast.im.MessageOuterClass.getDescriptor(),
           cool.scx.live_room_watcher.douyin.proto_entity.webcast.im.CommonOuterClass.getDescriptor(),
         });
     internal_static_UpdateFanTicketMessage_descriptor =
@@ -49,7 +47,6 @@ public final class UpdateFanTicketMessageOuterClass {
       com.google.protobuf.GeneratedMessageV3.FieldAccessorTable(
         internal_static_UpdateFanTicketMessage_descriptor,
         new java.lang.String[] { "Common", "RoomFanTicketCountText", "RoomFanTicketCount", "ForceUpdate", });
-    cool.scx.live_room_watcher.douyin.proto_entity.webcast.im.MessageOuterClass.getDescriptor();
     cool.scx.live_room_watcher.douyin.proto_entity.webcast.im.CommonOuterClass.getDescriptor();
   }
 
