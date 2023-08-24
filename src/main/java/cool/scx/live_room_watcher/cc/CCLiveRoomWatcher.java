03c26751e029584507fb284fdbcec8004e2d88c5
scx567888 <scx567888@outlook.com>
2023-08-24 18:31:09 +0800

    dev (#102)

---
+++ src/main/java/cool/scx/live_room_watcher/cc/CCLiveRoomWatcher.java
@@ -0,0 +1,23 @@
+package cool.scx.live_room_watcher.cc;
+
+import cool.scx.live_room_watcher.AbstractLiveRoomWatcher;
+
+/**
+ * 网易 CC 官方的获取方式
+ *
+ * @author scx567888
+ * @version 0.0.1
+ */
+public class CCLiveRoomWatcher extends AbstractLiveRoomWatcher {
+
+    @Override
+    public void startWatch() {
+
+    }
+
+    @Override
+    public void stopWatch() {
+
+    }
+    
+}
