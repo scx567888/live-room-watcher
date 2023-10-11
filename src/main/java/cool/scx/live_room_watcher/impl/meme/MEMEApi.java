884b7d4a35291f4b44e6182fa20ef692f54553c5
scx567888 <scx567888@outlook.com>
2023-10-11 13:30:25 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/meme/MEMEApi.java
+++ src/main/java/cool/scx/live_room_watcher/impl/meme/MEMEApi.java
@@ -2,17 +2,52 @@ package cool.scx.live_room_watcher.impl.meme;
 
 public class MEMEApi {
 
-    public static final String BASE_URL = "https://test-games.memeyule.com";
+    private String BASE_URL = "https://test-games.memeyule.com";
 
-//    public static final String WEBSOCKET_CHANNEL_URL = BASE_URL + "/websocket/channel";
+    private String WEBSOCKET_CHANNEL_URL = "https://test-games-sock.memeyule.com:6211/websocket";
 
-    public static final String BIND_ROOM_CODE_URL = BASE_URL + "/api/v1/openapi/bindRoomCode";
-    public static final String ROOM_CODE_STATUS_URL = BASE_URL + "/api/v1/openapi/roomCodeStatus";
-    public static final String STAR_INFO_URL = BASE_URL + "/api/v1/openapi/starInfo";
-    public static final String GIFTS_URL = BASE_URL + "/api/v1/openapi/gifts";
-    public static final String END_CALLBACK_URL = BASE_URL + "/api/v1/game/barrage/endCallback";
-    public static final String START_CALLBACK_URL = BASE_URL + "/api/v1/game/barrage/startCallback";
-    public static final String ACCESS_TOKEN_URL = BASE_URL+"/api/v1/openapi/accessToken";
+    public String WEBSOCKET_CHANNEL_URL() {
+        return WEBSOCKET_CHANNEL_URL;
+    }
 
+    public String BIND_ROOM_CODE_URL() {
+        return BASE_URL + "/api/v1/openapi/bindRoomCode";
+    }
+
+    public String ROOM_CODE_STATUS_URL() {
+        return BASE_URL + "/api/v1/openapi/roomCodeStatus";
+    }
+
+    public String STAR_INFO_URL() {
+        return BASE_URL + "/api/v1/openapi/starInfo";
+    }
+
+    public String GIFTS_URL() {
+        return BASE_URL + "/api/v1/openapi/gifts";
+    }
+
+    public String END_CALLBACK_URL() {
+        return BASE_URL + "/api/v1/game/barrage/endCallback";
+    }
+
+    public String START_CALLBACK_URL() {
+        return BASE_URL + "/api/v1/game/barrage/startCallback";
+    }
+
+    public String ACCESS_TOKEN_URL() {
+        return BASE_URL + "/api/v1/openapi/accessToken";
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
+
+    public void setWebSocketChannelUrl(String webSocketChannelUrl) {
+        this.WEBSOCKET_CHANNEL_URL = webSocketChannelUrl;
+    }
+
+    public String BASE_URL() {
+        return BASE_URL;
+    }
 
 }
