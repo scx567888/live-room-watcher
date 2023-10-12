556f753b4cf2c4be174f576e01670600a0e6baa9
scx567888 <scx567888@outlook.com>
2023-10-12 10:33:25 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/AccessToken.java
@@ -0,0 +1,9 @@
+package cool.scx.live_room_watcher;
+
+public interface AccessToken {
+
+    String accessToken();
+
+    Long expiresIn();
+
+}
---
+++ src/main/java/cool/scx/live_room_watcher/MsgType.java
@@ -0,0 +1,30 @@
+package cool.scx.live_room_watcher;
+
+public enum MsgType {
+
+    /**
+     * 评论
+     */
+    LIVE_COMMENT,
+
+    /**
+     * 礼物
+     */
+    LIVE_GIFT,
+
+    /**
+     * 点赞
+     */
+    LIVE_LIKE,
+
+    /**
+     * 粉丝团
+     */
+    LIVE_FANS_CLUB,
+
+    /**
+     * 关注
+     */
+    LIVE_FOLLOW
+
+}
