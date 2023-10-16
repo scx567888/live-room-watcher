58fdb7c43ac01ae495cae931da278efc3ed2d59c
scx567888 <scx567888@outlook.com>
2023-10-16 10:51:46 +0800

    dev (#135)

---
+++ src/main/java/cool/scx/live_room_watcher/impl/kuaishou/KuaiShouTaskStartResult.java
@@ -0,0 +1,7 @@
+package cool.scx.live_room_watcher.impl.kuaishou;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class KuaiShouTaskStartResult extends KuaiShouResponseBody {
+    public JsonNode userInfo;
+}
