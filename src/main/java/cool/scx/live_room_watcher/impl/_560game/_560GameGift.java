7026983543bd3ff159343b0526c7b3822cbb6eec
scx567888 <scx567888@outlook.com>
2024-01-11 17:09:37 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameGift.java
@@ -0,0 +1,54 @@
+package cool.scx.live_room_watcher.impl._560game;
+
+import cool.scx.live_room_watcher.message.Gift;
+import cool.scx.live_room_watcher.message.User;
+
+public class _560GameGift implements Gift, User {
+
+    public Integer MsgType;
+    public Integer GiftId;
+    public Integer GiftNum;
+    public Integer GameId;
+    public Long GiftPrice;
+    public String GiftName;
+    public String HeadImage;
+    public String Username;
+    public String GiftImage;
+    public String roomID;
+
+    @Override
+    public String name() {
+        return GiftName;
+    }
+
+    @Override
+    public long count() {
+        return GiftNum;
+    }
+
+    @Override
+    public String nickName() {
+        return Username;
+    }
+
+    @Override
+    public String avatar() {
+        return HeadImage;
+    }
+
+    @Override
+    public String userID() {
+        return Username;
+    }
+
+    @Override
+    public User user() {
+        return this;
+    }
+
+    @Override
+    public String roomID() {
+        return roomID;
+    }
+
+}
