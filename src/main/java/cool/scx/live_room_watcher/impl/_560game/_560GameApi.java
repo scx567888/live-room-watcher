f761c55c5f6356f51420c1cd339a94376c1f0df0
scx567888 <scx567888@outlook.com>
2024-01-11 17:09:59 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameApi.java
@@ -0,0 +1,7 @@
+package cool.scx.live_room_watcher.impl._560game;
+
+public class _560GameApi {
+    
+    public static final String VALIDATE_USER_URL = "/barrgame/grant_api/validateUser";
+    
+}
