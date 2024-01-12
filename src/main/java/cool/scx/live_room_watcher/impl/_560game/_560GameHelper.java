7ab6934cbd6154d40d904c27af486eb8b3916852
scx567888 <scx567888@outlook.com>
2024-01-12 10:20:16 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameHelper.java
@@ -0,0 +1,32 @@
+package cool.scx.live_room_watcher.impl._560game;
+
+import cool.scx.util.URIBuilder;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static cool.scx.constant.ScxDateTimeFormatter.yyyy_MM_dd;
+import static cool.scx.util.HashUtils.md5Hex;
+import static cool.scx.util.HashUtils.sha256Hex;
+
+public class _560GameHelper {
+
+    public static String getWsUrl(String baseUrl, String roomid) {
+        var data = yyyy_MM_dd.format(LocalDate.now()) + ":" + roomid;
+        var uriBuilder = URIBuilder.of(baseUrl)
+                .addParam("client_token", sha256Hex(data).toLowerCase())
+                .addParam("roomId", roomid);
+        return uriBuilder.toString();
+    }
+
+    public static String getSign(Map<String, String> map, String secret) {
+        var urlParams = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .map(k -> k.getKey() + "=" + k.getValue())
+                .collect(Collectors.joining("&"));
+        var s = urlParams + "&secret=" + secret;
+        return md5Hex(s).toUpperCase();
+    }
+
+}
