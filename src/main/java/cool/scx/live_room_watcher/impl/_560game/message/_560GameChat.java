ad390626136a6d61e96225b8717b80a2fa2fc37e
scx567888 <scx567888@outlook.com>
2024-01-12 10:03:28 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/_560game/message/_560GameChat.java
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/message/_560GameChat.java
@@ -1,46 +1,14 @@
 package cool.scx.live_room_watcher.impl._560game.message;
 
 import cool.scx.live_room_watcher.message.Chat;
-import cool.scx.live_room_watcher.message.User;
 
-public class _560GameChat implements Chat, User {
+public class _560GameChat extends _560GameUser implements Chat {
 
-    public Integer MsgType;
-    public Integer GameId;
     public String Content;
-    public String HeadImage;
-    public String Username;
-
-    public String roomID;
 
     @Override
     public String content() {
         return Content;
-    }
-
-    @Override
-    public String nickName() {
-        return Username;
-    }
-
-    @Override
-    public String avatar() {
-        return HeadImage;
-    }
-
-    @Override
-    public String userID() {
-        return Username;
-    }
-
-    @Override
-    public User user() {
-        return this;
-    }
-
-    @Override
-    public String roomID() {
-        return roomID;
     }
 
 }
--- src/main/java/cool/scx/live_room_watcher/impl/_560game/message/_560GameGift.java
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/message/_560GameGift.java
@@ -3,19 +3,13 @@ package cool.scx.live_room_watcher.impl._560game.message;
 import cool.scx.live_room_watcher.message.Gift;
 import cool.scx.live_room_watcher.message.User;
 
-public class _560GameGift implements Gift, User {
+public class _560GameGift extends _560GameUser implements Gift {
 
-    public Integer MsgType;
     public Integer GiftId;
     public Integer GiftNum;
-    public Integer GameId;
     public Long GiftPrice;
     public String GiftName;
-    public String HeadImage;
-    public String Username;
     public String GiftImage;
-
-    public String roomID;
 
     @Override
     public String name() {
@@ -25,31 +19,6 @@ public class _560GameGift implements Gift, User {
     @Override
     public long count() {
         return GiftNum;
-    }
-
-    @Override
-    public String nickName() {
-        return Username;
-    }
-
-    @Override
-    public String avatar() {
-        return HeadImage;
-    }
-
-    @Override
-    public String userID() {
-        return Username;
-    }
-
-    @Override
-    public User user() {
-        return this;
-    }
-
-    @Override
-    public String roomID() {
-        return roomID;
     }
 
 }
