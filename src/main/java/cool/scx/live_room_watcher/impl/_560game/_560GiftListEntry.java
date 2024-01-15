8b7f66e58cd2f96a76fcef5d7b674d22de53f39f
scx567888 <scx567888@outlook.com>
2024-01-15 15:29:50 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GiftListEntry.java
@@ -0,0 +1,9 @@
+package cool.scx.live_room_watcher.impl._560game;
+
+public class _560GiftListEntry {
+    public Integer gift_id;
+    public String gift_name;
+    public Integer gift_price;
+    public Integer type;
+    public String gift_icon;
+}
