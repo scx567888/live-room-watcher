ee3cd15be919c4a3e77b1ed4ee86a7ad82f1ff08
scx567888 <scx567888@outlook.com>
2024-03-06 16:06:21 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameApi.java
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameApi.java
@@ -4,5 +4,7 @@ public class _560GameApi {
     
     public static final String VALIDATE_USER_URL = "/barrgame/grant_api/validateUser";
     public static final String GET_GIFT_LIST_URL = "/barrgame/grant_api/getGiftList";
+    public static final String CLOSE_GAME_NOTIFY_URL = "/barrgame/grant_api/closeGameNotify";
+    public static final String REPORT_GAME_NOTIFY_URL = "/barrgame/grant_api/reportGameNotify";
     
 }
