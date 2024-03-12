413c6c3d3029968ae0e180b25a22fc2df8c19b4f
scx567888 <scx567888@outlook.com>
2024-03-12 13:07:23 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/AbstractLiveRoomWatcher.java
@@ -0,0 +1,81 @@
+package cool.scx.live_room_watcher;
+
+import cool.scx.live_room_watcher.message.*;
+
+import java.util.function.Consumer;
+
+/**
+ * <p>Abstract LiveRoomWatcher class.</p>
+ *
+ * @author scx567888
+ * @version 0.0.1
+ */
+public abstract class AbstractLiveRoomWatcher implements LiveRoomWatcher {
+
+    protected Consumer<Chat> onChat;
+    protected Consumer<Like> onLike;
+    protected Consumer<Gift> onGift;
+    protected Consumer<Follow> onFollow;
+    protected Consumer<User> onUser;
+
+    @Override
+    public LiveRoomWatcher onChat(Consumer<Chat> onChat) {
+        this.onChat = onChat;
+        return this;
+    }
+
+    @Override
+    public LiveRoomWatcher onLike(Consumer<Like> onLike) {
+        this.onLike = onLike;
+        return this;
+    }
+
+    @Override
+    public LiveRoomWatcher onGift(Consumer<Gift> onGift) {
+        this.onGift = onGift;
+        return this;
+    }
+
+    @Override
+    public LiveRoomWatcher onFollow(Consumer<Follow> onFollow) {
+        this.onFollow = onFollow;
+        return this;
+    }
+
+    @Override
+    public LiveRoomWatcher onUser(Consumer<User> onUser) {
+        this.onUser = onUser;
+        return this;
+    }
+
+    protected void _callOnChat(Chat chat) {
+        if (this.onChat != null) {
+            this.onChat.accept(chat);
+        }
+    }
+
+    protected void _callOnLike(Like like) {
+        if (this.onLike != null) {
+            this.onLike.accept(like);
+        }
+    }
+
+    protected void _callOnGift(Gift gift) {
+        if (this.onGift != null) {
+            this.onGift.accept(gift);
+        }
+    }
+
+    protected void _callOnFollow(Follow follow) {
+        if (this.onFollow != null) {
+            this.onFollow.accept(follow);
+        }
+    }
+
+    protected void _callOnUser(User user) {
+        if (this.onUser != null) {
+            this.onUser.accept(user);
+        }
+    }
+
+}
