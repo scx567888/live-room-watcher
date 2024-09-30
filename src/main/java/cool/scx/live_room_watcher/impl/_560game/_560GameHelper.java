dc73d7cab08f7f4cdb63f1db832c6ca24e38ca48
scx567888 <scx567888@outlook.com>
2024-09-30 19:32:11 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameHelper.java
+++ src/main/java/cool/scx/live_room_watcher/impl/_560game/_560GameHelper.java
@@ -1,12 +1,9 @@
 package cool.scx.live_room_watcher.impl._560game;
 
-import cool.scx.common.util.$;
-import cool.scx.common.util.URIBuilder;
 import cool.scx.http.uri.ScxURI;
 
 import java.time.LocalDate;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static cool.scx.common.util.HashUtils.md5Hex;
@@ -15,20 +12,11 @@ import static cool.scx.common.util.ScxDateTimeFormatter.yyyy_MM_dd;
 
 public class _560GameHelper {
 
-//    private static final HashedWheelTimer HASHED_WHEEL_TIMER = new HashedWheelTimer(Thread.ofVirtual().factory());
-//
-//    public static $.Timeout setTimeout(Runnable task, long delay) {
-//        return HASHED_WHEEL_TIMER.newTimeout((v) -> {
-//            task.run();
-//        }, delay, TimeUnit.MILLISECONDS);
-//    }
-
-    public static String getWsUrl(String baseUrl, String roomid) {
+    public static ScxURI getWsUrl(String baseUrl, String roomid) {
         var data = yyyy_MM_dd.format(LocalDate.now()) + ":" + roomid;
-        var uriBuilder = ScxURI.of(baseUrl)
+        return ScxURI.of(baseUrl)
                 .addQuery("client_token", sha256Hex(data).toLowerCase())
                 .addQuery("roomId", roomid);
-        return uriBuilder.toString();
     }
 
     public static String getSign(Map<String, String> map, String secret) {
