9c5bee42cc4d3fc4166a17b189bd5a646630c170
scx567888 <scx567888@outlook.com>
2026-06-23 18:41:25 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/douyin_hack/util/DouYinHackHelper.java
+++ src/main/java/cool/scx/live_room_watcher/impl/douyin_hack/util/DouYinHackHelper.java
@@ -4,6 +4,8 @@ import com.microsoft.playwright.BrowserType.LaunchOptions;
 import com.microsoft.playwright.Playwright;
 import cool.scx.live_room_watcher.impl.douyin_hack.entity.DouYinAPP;
 import dev.scx.http.headers.cookie.Cookie;
+import dev.scx.node.ArrayNode;
+import dev.scx.node.StringNode;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.HostAccess;
@@ -13,6 +15,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static dev.scx.serialize.ScxSerialize.fromJson;
+import static dev.scx.serialize.ScxSerialize.nodeToObject;
 
 public final class DouYinHackHelper {
 
@@ -26,50 +29,35 @@ public final class DouYinHackHelper {
         try (context) {
             var parse = Jsoup.parse(htmlStr);
             var scripts = parse.select("script");
-            // 重写 window, self 等, 让 removeChild 成为 nop 方法.
+            // 重写 self.
             context.eval("js", """
-                var window = {};
-                var self = window;
-                var document = {
-                    scripts: [
-                        {
-                            parentNode: {
-                                removeChild(e) {
-
-                                }
-                            }
-                        }
-                    ]
-                };
+                var self = { __pace_f : []};
                 """);
-            // 执行 脚本中的 方法
+            // 执行 脚本中的 方法, 我们主要是为了获得 self.__pace_f
             for (var script : scripts) {
                 var html = script.html();
-                if (html.startsWith("self.__pace_f") || html.startsWith("(self.__pace_f")) {
+                if (html.startsWith("self.__pace_f")) {
                     context.eval("js", html);
                 }
             }
-            // 解析 最终的 json
+            // 获取 self.__pace_f
             var value = context.eval("js", """
-                var r;
-
-                function m(e) {
-                    if (0 === e[0]) {
-                        r = [];
-                    } else {
-                        if (r) {
-                            r.push(e[1]);
-                        }
-                    }
-                }
-
-                self.__pace_f.forEach(m);
-                let str = r[r.length - 1];
-                let index = str.indexOf(":");
-                let arr = JSON.parse(str.slice(index + 1));
-                JSON.stringify(arr[arr.length - 1])
+                JSON.stringify(self.__pace_f)
                 """);
-            return fromJson(value.asString(), DouYinAPP.class);
+            var self__pace_f = (ArrayNode) fromJson(value.asString());
+            // 取最后一组
+            var node1 = (ArrayNode) self__pace_f.get(self__pace_f.size() - 1);
+            // 取 第二个值
+            var node2 = (StringNode) node1.get(1);
+            var str1 = node2.value();
+            // 移除前面的 "c:"
+            var str2 = node2.value().substring(str1.indexOf(":") + 1);
+            // 这里其实也是数组 我们取最后一个值
+            var node3 = (ArrayNode) fromJson(str2);
+            // 这才是最终的值
+            var node4 = node3.get(node3.size() - 1);
+            // 转换成结构化对象
+            return nodeToObject(node4, DouYinAPP.class);
         }
     }
 
