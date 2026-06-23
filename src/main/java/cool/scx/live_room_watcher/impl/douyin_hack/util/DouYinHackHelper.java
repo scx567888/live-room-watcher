63c3282ec5d1ceb2bc3a8e9bf42eebd0de65a07c
scx567888 <scx567888@outlook.com>
2026-06-23 18:14:00 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/douyin_hack/util/DouYinHackHelper.java
+++ src/main/java/cool/scx/live_room_watcher/impl/douyin_hack/util/DouYinHackHelper.java
@@ -8,9 +8,6 @@ import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Engine;
 import org.graalvm.polyglot.HostAccess;
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -24,35 +21,47 @@ public final class DouYinHackHelper {
 
     /// 从 body 中解析出 DouYinAPP
     public static DouYinAPP parseDouYinAPPByHtml(String htmlStr) {
-        Context context = Context.newBuilder().allowHostAccess(HOST_ACCESS).engine(ENGINE).build();
+        var context = Context.newBuilder().allowHostAccess(HOST_ACCESS).engine(ENGINE).build();
         try (context) {
-            Document parse = Jsoup.parse(htmlStr);
-            Elements scripts = parse.select("script");
+            var parse = Jsoup.parse(htmlStr);
+            var scripts = parse.select("script");
+            // 重写 window, self 等, 让 removeChild 成为 nop 方法.
             context.eval("js", """
-                var window= {};
-                var self= window;
-                var document={
-                        scripts:[{
-                            parentNode:{removeChild(e){}}
-                        }]};
+                var window = {};
+                var self = window;
+                var document = {
+                    scripts: [
+                        {
+                            parentNode: {
+                                removeChild(e) {
+
+                                }
+                            }
+                        }
+                    ]
+                };
                 """);
-            for (Element script : scripts) {
-                String html = script.html();
+            // 执行 脚本中的 方法
+            for (var script : scripts) {
+                var html = script.html();
                 if (html.startsWith("self.__pace_f") || html.startsWith("(self.__pace_f")) {
                     context.eval("js", html);
                 }
             }
+            // 解析 最终的 json
             var value = context.eval("js", """
                 var r;
+
                 function m(e) {
-                   if (0 === e[0])
-                       r = [];
-                   else {
-                       if (r){
-                           r.push(e[1]);
-                       }
-                   }
+                    if (0 === e[0]) {
+                        r = [];
+                    } else {
+                        if (r) {
+                            r.push(e[1]);
+                        }
+                    }
                 }
+
                 self.__pace_f.forEach(m);
                 let str = r[r.length - 1];
                 let index = str.indexOf(":");
@@ -60,8 +69,6 @@ public final class DouYinHackHelper {
                 JSON.stringify(arr[arr.length - 1])
                 """);
             return fromJson(value.asString(), DouYinAPP.class);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("解析直播间错误", e);
         }
     }
 
@@ -76,10 +83,11 @@ public final class DouYinHackHelper {
 
                 var webSocketOptions = new WebSocketOptions(c.url(), context.cookies().stream().map(cookie -> Cookie.of(cookie.name, cookie.value)).toArray(Cookie[]::new));
                 future.complete(webSocketOptions);
+
             });
             page.navigate(path);
             page.waitForWebSocket(() -> {
-                //什么也不做
+                // 什么也不做
             });
         }
         try {
