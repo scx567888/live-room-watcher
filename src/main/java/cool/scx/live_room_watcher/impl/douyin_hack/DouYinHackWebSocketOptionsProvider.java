9f6f87f2f643ef7c3d4b528c425c703bc6986486
scx567888 <scx567888@outlook.com>
2026-06-25 14:44:00 +0800

    dev

---
+++ src/main/java/cool/scx/live_room_watcher/impl/douyin_hack/DouYinHackWebSocketOptionsProvider.java
@@ -0,0 +1,8 @@
+package cool.scx.live_room_watcher.impl.douyin_hack;
+
+import cool.scx.live_room_watcher.impl.douyin_hack.util.Browser;
+import dev.scx.function.Function1;
+
+public interface DouYinHackWebSocketOptionsProvider extends Function1<Browser, DouYinHackWebSocketOptions, Exception> {
+
+}
