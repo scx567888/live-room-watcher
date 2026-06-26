9613a1ba7e2eb24f3cf0bcafe22ff1594adfea12
scx567888 <scx567888@outlook.com>
2026-06-26 15:16:30 +0800

    dev

--- src/main/java/cool/scx/live_room_watcher/impl/kuaishou/KuaiShouTaskStartResult.java
+++
@@ -1,9 +0,0 @@
-package cool.scx.live_room_watcher.impl.kuaishou;
-
-import dev.scx.node.Node;
-
-public class KuaiShouTaskStartResult extends KuaiShouResponseBody {
-
-    public Node userInfo;
-
-}
